28f7d361840393a2fd9750577c354348266ae80c
Ste <24140520@edgehill.ac.uk>
2021-05-03 02:38:11 +0100

    json files updated, more pages added and pages linked properly. json calls working

--- module.js
+++ module.js
@@ -1,66 +1,44 @@
-// var pageCounter = 1;
-// var moduleContainer = document.getElementById('module-info');
-var degreeBtn = document.getElementById("degreeBtn");
+var moduleContainer = document.getElementById('module-info');
 var moduleBtn = document.getElementById("moduleBtn");
-var assessmentBtn = document.getElementById("assessmentBtn");
 
-degreeBtn.addEventListener("click", function(){
+moduleBtn.addEventListener("click", function(){
   var ourRequest = new XMLHttpRequest();
   ourRequest.open('GET', 'https://raw.githubusercontent.com/SteColeman/CIS2169CW2/master/module.json');
   ourRequest.onload = function(){
-    //console.log(ourRequest.responseText);
     var ourData = JSON.parse(ourRequest.responseText);
-    //console.log(ourData[0]);
     renderHTML(ourData);
   };
   ourRequest.send();
-// pageCounter++;
-// if (pageCounter > 3){
-// //btn.classList.add("hide-me");
-//   btn.disabled = true;
-// }
+
 });
 
 function renderHTML(data){
   var htmlString = "";
 
   for(i = 0; i < data.length; i++){
-    htmlString += "<p>" + data[i].Name + " is a " + data[i].Course + " has assements "; //".</p>";
-    for(ii = 0; ii < data[i].Module.Assignment.length; ii++){
+    htmlString += "<p>" + data[i].ID_Code + " - " + data[i].Name + " is worth a total of "
+    + data[i].Information.Credits + " credits. It contains " + data[i].Information.Learning_outcomes.length
+     + " Learning Outcomes and has " + data[i].Information.Learning_outcomes.length + " assessments " ; //".</p>";
+
+    htmlString += " The Learning Outcome's are ";
+    for(ii = 0; ii < data[i].Information.Learning_outcomes.length; ii++){
       if (ii == 0){
-        htmlString += data[i].Module.Assignment[ii];
+        htmlString += data[i].Information.Learning_outcomes[ii];
       } else {
-        htmlString += " and " + data[i].Module.Assignment[ii];
-      }
-    }
-    htmlString += ' and Learning Outcome ';
-    for(ii = 0; ii < data[i].Module.Learning_outcomes.length; ii++){
-      if (ii == 0){
-        htmlString += data[i].Module.Learning_outcomes[ii];
-      } else {
-        htmlString += " and " + data[i].Module.Learning_outcomes[ii];
+        htmlString += " and " + data[i].Information.Learning_outcomes[ii];
       }
     }
 
     htmlString += ' and Volume ';
-    for(ii = 0; ii < data[i].Module.Volume.length; ii++){
+    for(ii = 0; ii < data[i].Information.Assessments.length; ii++){
       if (ii == 0){
-        htmlString += data[i].Module.Volume[ii];
+        htmlString += data[i].Information.Assessments[ii];
       } else {
-        htmlString += " and " + data[i].Module.Volume[ii];
+        htmlString += " and " + data[i].Information.Assessments[ii];
       }
     }
-
-    htmlString += ' and weights ';
-    for(ii = 0; ii < data[i].Module.weights.length; ii++){
-      if (ii == 0){
-        htmlString += data[i].Module.weights[ii];
-      } else {
-        htmlString += " and " + data[i].Module.weights[ii];
-      }
-    }
-    htmlString += '.</p>';
   }
+
   moduleContainer.insertAdjacentHTML('beforeend', htmlString);
 
 }
