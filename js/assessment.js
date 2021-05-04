97ce2e73c4daa22ef3cb11551e200bb251853ef7
Ste <24140520@edgehill.ac.uk>
2021-05-04 04:56:24 +0100

    spelling errors fixed and final styling changes made

--- js/assessment.js
+++ js/assessment.js
@@ -3,7 +3,7 @@ var assessmentBtn = document.getElementById("assessmentBtn");
 
 assessmentBtn.addEventListener("click", function(){
   var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'https://raw.githubusercontent.com/SteColeman/CIS2169CW2/master/json/timeslot.json');
+  ourRequest.open('GET', 'https://raw.githubusercontent.com/SteColeman/CIS2169CW2/master/json/assessment.json');
   ourRequest.onload = function(){
     var ourData = JSON.parse(ourRequest.responseText);
     renderHTML(ourData);
@@ -18,9 +18,9 @@ function renderHTML(data){
   for(i = 0; i < data.length; i++){
     htmlString += "<p>" + data[i].ID_Code + " - " + data[i].Module_name + " - " +
     data[i].Assessment_info.Title + ", is assessment number  " + data[i].Assessment_info.Number +
-    "covering Learning Outcomes " + data[i].Assessment_info.LO_overed + " with a volume of " + 
+    " covering Learning Outcomes " + data[i].Assessment_info.LO_covered + " with a volume of " + 
     data[i].Assessment_info.Volume + " and a weighting of " + data[i].Assessment_info.Weighting + 
-    ". The Submission date is " + data[i].Assessment_info.Submission_date; 
+    " for the module. The Submission date is " + data[i].Assessment_info.Submission_date + ".";
   }
 
   moduleContainer.insertAdjacentHTML('beforeend', htmlString);
